b09b9bbbd1113d2cf4b1ef869906eb3b8d32fcbe
Chris Reister <chris@chrisreister.com>
2012-08-23 16:50:59 -0700

    alter seeds to be a little cleaner and 2.06-ish

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,19 @@ Refinery::User.all.each do |user|
     user.plugins.create(:name => 'videos',
                         :position => (user.plugins.maximum(:position) || -1) +1)
   end
-end
+end if defined?(Refinery::User)
 
-if defined?(Refinery::Page) && Refinery::Page.where(:link_url => "/videos").first.nil?
+if defined?(Refinery::Page) && !Refinery::Page.exists?(:link_url => "/videos")
   page = Refinery::Page.create(
     :title => 'Videos',
     :link_url => '/videos',
     :deletable => false,
     :show_in_menu => false,
-    :position => ((Refinery::Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1),
+    #:position => ((Refinery::Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1),
     :menu_match => '^/videos(\/|\/.+?|)$'
   )
+  
+  Refinery::Pages.default_parts.each do |default_page_part|
+    page.parts.create(:title => default_page_part, :body => nil)
+  end
 end
