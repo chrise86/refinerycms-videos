8649442f6ddcdb2834d7def3d3a02e8da90c8fbd
Chris Reister <chris@chrisreister.com>
2012-09-05 16:33:08 -0700

    move helper to the Refinery::VIdeos namespace

---
+++ app/helpers/refinery/videos/raw_videos_helper.rb
@@ -0,0 +1,18 @@
+module Refinery
+  module Videos
+    module RawVideosHelper
+      def render_video_player(raw_video)
+        video_urls = RawVideo.html5_sort(raw_video.encoded_videos).collect do |encoded_video|
+          encoded_video.remote_url
+        end
+
+        video_tag(video_urls,
+          :class => "sublime",
+          :size => "768x428",
+          :poster => raw_video.poster_image_url,
+          :preload => "none"
+        )
+      end
+    end
+  end
+end
